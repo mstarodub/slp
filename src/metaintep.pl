91ebde25ca6495d6dc84a6040cf790537b977b94
unknown <R.Schwank@campus.lmu.de>
2023-08-10 22:09:08 +0200

    fix duplicate outputs, now 'false'

--- src/metaintep.pl
+++ src/metaintep.pl
@@ -6,33 +6,34 @@
 % lesen von probabilities
 :- op(1199,xfx,::).
 
-demo(true, 1). % base case 'Rule = true'
+demo(true, 1) :- !. % base case 'Rule = true'
 
 demo(Rule, Prob) :- % base case 'body of Rule = true', i.e. Rule atomic
     clause(Prob :: Rule, true).
 
 demo(Rule, ProbRes) :- % case 'compounded body, conjunction'
     clause((PRet :: Rule), Body),
-    Body = (Goal1, Goal2), % !, cut needed?, cut operator in correct place? needs to prevent unifying with 'single predicate body' case as well
-    !,
+    Body = (Goal1, Goal2), 
+    !, % cut prevents unifying with 'single predicate body' case as well
     demo(Goal1, Prob1),
     demo(Goal2, Prob2),
     ProbRes is PRet*Prob1*Prob2.
 
 demo(Rule, ProbRes) :- % case 'compounded body, disjunction'
     clause((PRet :: Rule), Body),
-    Body = (Goal1; Goal2), % !, cut needed?, cut operator in correct place? needs to prevent unifying with Body = Goal case as well
-    !,
+    Body = (Goal1; Goal2), 
+    !, % cut prevents unifying with Body = Goal case as well
     demo(Goal1, Prob1),
     demo(Goal2, Prob2),
     ProbRes is PRet*(Prob1 + Prob2 - Prob1*Prob2).
 
 demo(Rule, ProbRes) :- % case 'single predicate body'
     clause((PRet :: Rule), Body),
-    Body = Goal,
-    !,
+    ( Body = true
+    -> fail, !
+    ; Body = Goal,
     demo(Goal, Prob),
-    ProbRes is PRet*Prob.
+    ProbRes is PRet*Prob).
 
 % not yet tested
 
@@ -41,6 +42,7 @@ demo(Rule, ProbRes) :- % case 'single predicate body'
 
 0.4 :: q(a).
 0.5 :: q(b).
+0.7 :: q(c).
 
 % ok
 0.5::s(X) :- q(X).
