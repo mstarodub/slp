6c500020aa3d355b4aa4f97bc90c5da116a0d843
mxs <mxs@dismail.de>
2023-09-13 21:14:10 +0200

    wip: clause selector

--- src/cussens-inference.pl
+++ src/cussens-inference.pl
@@ -104,6 +104,36 @@ z(G, Weight) :-
     unifSet_rec(G, true, UnifSet, Weight).
 
 
+% compound body
+sample((G1, G2)) :-
+    !,
+    sample(G1),
+    sample(G2).
+
+sample(G) :-
+    \+clause(G, _),
+    !,
+    G.
+
+sample(Head) :-
+    findall([Prob, Head, Body], clause((Prob :: Head), Body), Bag),
+    random_clause(Head, Body, Bag),
+    !,
+    sample(Body).
+
+% probabilistic clause selector
+random_clause(Head, Body, Bag) :-
+    Rand is random_float,
+    choose(Bag, Head, Body, 0, Rand, Sum).
+choose([], _, _, _, _, 0).
+choose([[Prob, Head, Body]|Tail], Head1, Body1, Akk, Rand, Rest) :-
+    Akknew is Akk + Prob,
+    choose(Tail, Head1, Body1, Akknew, Rand, Rest1),
+    Rest is Rest1 + Prob,
+    ((var(Body1),
+        Prob1 is Akk/(Akk+Rest), Rand >= P1, Head1 = Head, Body1 = Body);
+    true).
+
 % TESTS
 % want: z((q(X), p(X)), W) === 0.46
 0.6 :: p(a).
