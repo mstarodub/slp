9fd3021a41d46de95dfd4aebdfd6e9926cc4defe
unknown <R.Schwank@campus.lmu.de>
2023-09-19 21:47:24 +0200

    marginal inference derived from success contraint inference; unpleasant looking result

--- src/cussens-inference.pl
+++ src/cussens-inference.pl
@@ -11,13 +11,17 @@
 %   G is non-compound; TODO: assert user input as clause and retract later
 %   ArgList contains the instantiated parameters for G
 
+inference_marginal(Goal, BindingProbList) :- z(Goal, BindingProbList, 0).
+
+
+
 % want p(f(b), P)
 % -> p(f(X), [X=b], P).
 p(G, ArgList, RoundedResult) :-
     copy_term(G, GFree),
     unify_helper(G, ArgList),
-    z(G, Numerator),
-    z(GFree, Denominator),
+    z(G, Numerator, _),
+    z(GFree, Denominator, _),
     Result is Numerator / Denominator,
     round_third(Result, RoundedResult).
 
@@ -52,8 +56,8 @@ replace_args([Arg|Rest],Find,Replacement,[ReplacedArg|ReplacedRest]) :-
 p(G, RoundedResult) :-
     %as_vars(G, GFree),
     replace(G, b, X, GFree),
-    z(G, Numerator),
-    z(GFree, Denominator),
+    z(G, Numerator, 1),
+    z(GFree, Denominator, 1),
     Result is Numerator / Denominator,
     round_third(Result, RoundedResult).
 
@@ -78,8 +82,8 @@ round_third(Float, RoundedFloat) :-
     RoundedScaled is round(Float*1000),
     RoundedFloat is RoundedScaled/1000. 
 
-unifSet_rec(_, _, [], 0) :- !. % base case cut: prevents further backtracking and final output "false"
-unifSet_rec(CurrentGoal, RemainingGoal, [UnifClause|UnifSetTail], Akk) :-
+unifSet_rec(_, _, [], 0, _) :- !. % base case cut: prevents further backtracking and final output "false"
+unifSet_rec(CurrentGoal, RemainingGoal, [UnifClause|UnifSetTail], Akk, Depth) :-
     % unifSet_rec recursion requires all subgoals to be as unbound as possible
     % free variable relations must be preserved, e.g. p(X), q(X) must become p(Y), q(Y)
     copy_term((CurrentGoal,RemainingGoal, UnifSetTail), (CurrentGoalFree,RemainingGoalFree, UnifSetTailFree)),
@@ -89,12 +93,18 @@ unifSet_rec(CurrentGoal, RemainingGoal, [UnifClause|UnifSetTail], Akk) :-
     nth0(2, UnifClause, ClauseBody),
     unifiable(ClauseHead, CurrentGoal, UnifBag),
     unify_helper(RemainingGoal, UnifBag), % how should unify_helper behave if variables in UnifBag have more unification options?
-    z((ClauseBody, RemainingGoal), Weight),
-    unifSet_rec(CurrentGoalFree, RemainingGoalFree, UnifSetTailFree, Akknew),
-    Akk is ClauseProb*Weight + Akknew.
+    DepthNew is Depth+1,
+    z((ClauseBody, RemainingGoal), Weight, DepthNew),
+    unifSet_rec(CurrentGoalFree, RemainingGoalFree, UnifSetTailFree, Akknew, Depth),
+    ( Depth = 0
+        ->  BindingProb is ClauseProb*Weight,
+            round_third(BindingProb, BindingProbRound), % rounding probability occurring in output
+            append(UnifBag, [BindingProbRound], AkkTemp),
+            ( \+ integer(Akknew) -> append(AkkTemp, Akknew, Akk); Akk = AkkTemp ) % for all but the last element in unif set Akknew will be a list
+        ;   Akk is ClauseProb*Weight + Akknew).
 
-substitSet_rec(_, _, [], 0) :- !. % base case cut: prevents further backtracking and final output "false"
-substitSet_rec(CurrentGoal, RemainingGoal, [Substitutions|PairedVarBindingsTail], Akk) :-
+substitSet_rec(_, _, [], 0, _) :- !. % base case cut: prevents further backtracking and final output "false"
+substitSet_rec(CurrentGoal, RemainingGoal, [Substitutions|PairedVarBindingsTail], Akk, Depth) :-
     % substitSet_rec recursion requires all subgoals to be as unbound as possible
     % PairedVarBindings must also remain unbound, e.g. old: [X=a], [X=b] becomes [Y=b] instead of [a=b]
     % free variable relations must be preserved, e.g. p(X), q(X) must become p(Y), q(Y)
@@ -102,46 +112,53 @@ substitSet_rec(CurrentGoal, RemainingGoal, [Substitutions|PairedVarBindingsTail]
 
     unify_helper(CurrentGoal, Substitutions),
     unify_helper(RemainingGoal, Substitutions),
-    z(CurrentGoal, Weight1),
-    z(RemainingGoal, Weight2),
-    substitSet_rec(CurrentGoalFree, RemainingGoalFree, PairedVarBindingsTailFree, Akknew),
+    z(CurrentGoal, Weight1, Depth),
+    z(RemainingGoal, Weight2, Depth),
+    substitSet_rec(CurrentGoalFree, RemainingGoalFree, PairedVarBindingsTailFree, Akknew, Depth),
     Akk is Weight1*Weight2 + Akknew.
 
 % base case
-z(true, 1).
+z(true, 1, _).
 
 % compound base cases; simplifying conjunction
 % base case cuts: prevent further backtracking and final output "false"
-z((G, true), Weight) :- z(G, Weight), !. % fires for G as body of a non-compound head
-z((true, G), Weight) :- z(G, Weight), !.
+z((G, true), Weight, Depth) :- z(G, Weight, Depth), !. % fires for G as body of a non-compound head
+z((true, G), Weight, Depth) :- z(G, Weight, Depth), !.
 
 % compound head
-z((G1, G2), Weight) :-
+z((G1, G2), Weight, Depth) :-
     G1 \= true, % mutual exclusivity of goals
 
-    % shared variable test
-    sub_term_shared_variables(G1, (G1, G2), SharedVars),
-    ( SharedVars = [] 
-    % no shared variables --> decomposition
-    ->  z(G1, Weight1),
-        z(G2, Weight2),
-        Weight is Weight1*Weight2
-    % shared variables --> computation of splitting substitution set
-    ;   % only ground terms make goals disjunct
-        findall(SharedVars, (clause((_ :: G1), _), ground(SharedVars)), SubstitList),
-        list_to_set(SubstitList, SubstitSet),
-        map_singleton(unifiable, [SharedVars], SubstitSet, PairedVarBindings),
-        substitSet_rec(G1, G2, PairedVarBindings, Weight)        
+    % marginal inference --> standard not optimised z computation (with nonvar Depth)
+    ( nonvar(Depth) 
+    ->  findall([Prob, G1, Body], clause((Prob :: G1), Body), UnifSet),
+        unifSet_rec(G1, G2, UnifSet, Weight, Depth)
+    % success constraint inference --> optimised z computation (with var Depth)
+    ;   % shared variable test
+        sub_term_shared_variables(G1, (G1, G2), SharedVars),
+        ( SharedVars = [] 
+        % no shared variables --> decomposition
+        ->  z(G1, Weight1, Depth),
+            z(G2, Weight2, Depth),
+            Weight is Weight1*Weight2
+        % shared variables --> computation of splitting substitution set
+        ;   % only ground terms make goals disjunct
+            findall(SharedVars, (clause((_ :: G1), _), ground(SharedVars)), SubstitList),
+            list_to_set(SubstitList, SubstitSet),
+
+            map_singleton(unifiable, [SharedVars], SubstitSet, PairedVarBindings),
+            substitSet_rec(G1, G2, PairedVarBindings, Weight, Depth)        
+        )
     ).
 
 % non-compound head
-z(G, Weight) :-
+z(G, Weight, Depth) :-
     % mutual exclusivity of goals
     G \= (_, _),
     G \= true,
 
     findall([Prob, G, Body], clause((Prob :: G), Body), UnifSet),
-    unifSet_rec(G, true, UnifSet, Weight).
+    unifSet_rec(G, true, UnifSet, Weight, Depth).
 
 
 % compound body
@@ -199,3 +216,5 @@ choose([[Prob, Head, Body]|Tail], Head1, Body1, Akk, Rand, Rest) :-
 0.2 :: s(b, b).
 0.2 :: r(b, Z) :- p(Z).
 0.8 :: r(a, b).
+
+% current state: inference_marginal((s(X,Y), r(Y,Z)), Prob) results in Y=b, Prob = [b=b, _=X, 0.14, c=c, _=a, 0, b=b, _=b, 0.04] ; false.
